30f07c208e8eb39fd9b0ceacdd4c89481a650736
BekzodbekRamatov <bek847531@gmail.com>
2025-09-19 17:35:24 +0500

    2-dars php

---
+++ 2-dars.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <pre>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, vero obcaecati? Exercitationem 
+            enim esse temporibus adipisci natus, culpa doloremque provident incidunt cupiditate. Ea illo
+             quisquam expedita dicta iusto error, sint atque, corrupti repudiandae, ipsam eius. Beatae eum 
+             nesciunt, similique dolores dicta soluta, impedit facilis, esse reiciendis debitis consequuntur
+             dolorum deleniti?
+        </pre>
+    </div>
+</body>
+
+</html>
